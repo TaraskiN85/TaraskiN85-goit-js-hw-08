3ebeb47f3afc97e22e0f99f1addbe4d0f533593c
TaraskiN85 <taras12304@i.ua>
2023-10-23 12:44:32 +0300

    accomplish task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const currentTime = localStorage.getItem('videoplayer-current-time');
+const player = new Player(iframe);
+
+const onPlay = data => {
+  localStorage.setItem('videoplayer-current-time', `${data.seconds}`);
+};
+
+player.setCurrentTime(currentTime);
+player.on('timeupdate', throttle(onPlay, 1000));
