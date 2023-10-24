9cbe1e6bff09d9c294873fa55afedfc260aea7a4
TaraskiN85 <taras12304@i.ua>
2023-10-25 01:59:25 +0300

    change package.json to deploy

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,18 @@
+import throttle from 'lodash.throttle';
+const form = document.querySelector('.feedback-form');
+// const email = form.elements;
+const [email, message, ...rest] = [...form.elements];
+// const message = feedbackForm.textarea.value;
 
+const userData = {
+  email: email.value,
+  message: message.value,
+};
+
+console.dir(email);
+const updateData = event => {
+  const data = event.target.value;
+  // localstorage;
+  console.log(data);
+};
+// form.addEventListener('input', throttle(updateData, 500));
